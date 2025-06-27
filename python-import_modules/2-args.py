481f56929c6af81a7710efe562100056412d2146
teejayk <t.kongolo@alustudent.com>
2025-06-27 22:49:16 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Script to display command line arguments with count."""
 from sys import argv
+
+"""Script to display command line arguments with count."""
 
 
 if __name__ == "__main__":
     num_args = len(argv) - 1
     args = argv[1:]
+
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
     else:
         print(f"{num_args} arguments:")
+
     for i, arg in enumerate(args, start=1):
         print(f"{i}: {arg}")
