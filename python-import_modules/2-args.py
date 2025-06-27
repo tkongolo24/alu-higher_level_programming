8843f1a3d295b989e3d6a12dfd9921767c702367
root <t.kongolo@alustudent.com>
2025-06-27 13:28:14 -0700

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
+from calculator_1 import add, sub, mul, div  
+
+a = 10
+b = 5
 
 if __name__ == "__main__":
-    num_args = len(argv) - 1
-    args = argv[1:]
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
-    if num_args == 0:
-        print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument:")
-    else:
-        print(f"{num_args} arguments:")
-
-    for i, arg in enumerate(args, start=1):
-        print(f"{i}: {arg}")
