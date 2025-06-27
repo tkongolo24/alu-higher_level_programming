8081168826ef713677251dee7656e98001302181
root <t.kongolo@alustudent.com>
2025-06-27 03:39:00 -0700

    1

---
+++ python-import_modules/main1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, subtract, multiply, divide
+
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
+
