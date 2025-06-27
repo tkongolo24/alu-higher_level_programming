58dee30acb4f791412fdaf042ffcd4fce75e8309
root <t.kongolo@alustudent.com>
2025-06-27 00:44:26 -0700

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(i)) for i in range(97, 123) if i != 101 and i != 113), end="")
+print("".join("{}".format(chr(i)) for i in range(97, 123)
+      if i != 101 and i != 113), end="")
