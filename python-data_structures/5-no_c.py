fcf601bc2d91707608790ce3177fb2087cb34080
root <t.kongolo@alustudent.com>
2025-06-27 06:30:44 -0700

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function that removes all 'c' and 'C' characters from a string."""
+
+
 def no_c(my_string):
     """Return a new string with all 'c' and 'C' characters removed."""
     return ''.join([char for char in my_string if char != 'c' and char != 'C'])
