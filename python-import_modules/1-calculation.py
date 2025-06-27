9f65804e552b270472147bea755fd5e44252c59c
teejayk <t.kongolo@alustudent.com>
2025-06-27 22:31:22 +0200

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
-
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
-
-
 from calculator_1 import add, sub, mul, div
+
 
 a = 10
 b = 5
@@ -26,4 +10,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
