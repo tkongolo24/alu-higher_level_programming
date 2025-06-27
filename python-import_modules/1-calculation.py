df62d2f3a7cdfa0e28f2f208272d67932370ffca
root <t.kongolo@alustudent.com>
2025-06-27 03:51:58 -0700

    1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+
 def add(a, b):
     return a + b
+
 
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
 
+
 def divide(a, b):
     return a / b
+
 
 from calculator_1 import add, sub, mul, div
 
