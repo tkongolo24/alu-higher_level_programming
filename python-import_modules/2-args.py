0f2ef5d7e197ca0765089f50536e056c673941f8
teejayk <t.kongolo@alustudent.com>
2025-06-27 22:56:26 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+from sys import argv
 
-import sys
-
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    num_args = len(args)
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    args = argv[1:]
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
-        print("1: " + args[0])
     else:
         print("{} arguments:".format(num_args))
-        for i in range(len(args)):
-            print("{}: {}".format(i+1, args[i]))
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
