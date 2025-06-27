1ce250104468ca09f3cb483b933c203afed58fab
root <t.kongolo@alustudent.com>
2025-06-27 00:36:49 -0700

    2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(chr(i), end="")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
